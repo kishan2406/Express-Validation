4748616bb26df84fe35d4c44426acc46e7beb4d1
KISHAN PRAJAPATI <97467307+kishan2406@users.noreply.github.com>
2022-03-18 01:31:55 +0530

    Delete configs directory

--- configs/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose=require("mongoose")
-module.exports=()=>{
-    return mongoose.connect("mongodb+srv://KishanPrajapati:<password>@cluster0.77mkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-};
